4b2c02de2ed9c79eda4eedae474a8c93adbd8c8d
makoto yaguchi <akmiccom@gmail.com>
2025-02-17 10:25:19 +0900

    First commit

---
+++ Basic_of_JaveScript/script.js
@@ -0,0 +1,18 @@
+// console.log("Hello World");
+
+// function sum(a, b){
+//     return a+b;
+// }
+// console.log(sum(2, 3));
+
+for (let i = 0; i < 20; i++) {
+    let output = "";
+    if (i % 3 === 0) output += "Fizz";
+    if (i % 5 === 0) output += "Buzz";
+    console.log(`${output}` || i);
+}
+
+while (true) {
+    let name = prompt("Mako");
+
+}
